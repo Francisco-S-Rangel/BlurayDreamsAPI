fb775eb51753b3c14f0ba0cd19d5f083acdf3c11
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-03-14 22:55:09 -0300

    novas entidades foram entidades

---
+++ Models/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlurayDreamsAPI.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Img { get; set; }
+        [Required]
+        public string tipo { get; set; }
+        [Required]
+        public string Categoria { get; set; }
+        [Required]
+        public string Ano { get; set; }
+        [Required]
+        public string Direcao { get; set; }
+        [Required]
+        public string Duracao { get; set; }
+        [Required]
+        public string Produtora { get; set; }
+        [Required]
+        public string Sinopse { get; set; }
+
+
+    }
+}
