3dcda77b25671bda69a5edb49351025447158167
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-04-02 21:24:15 -0300

    Novos Models e algumas melhorias

---
+++ BusinessModels/DesativacaoClienteModel.cs
@@ -0,0 +1,24 @@
+﻿using BlurayDreamsAPI.Models;
+using System.Text.Json.Serialization;
+
+namespace BlurayDreamsAPI.BusinessModels
+{
+    public class DesativacaoClienteModel
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public string MotivoDesativacao { get; set; }
+        [JsonIgnore]
+        public Cliente? Cliente { get; set; }
+
+        public DesativacaoCliente toEntity()
+        {
+            return new DesativacaoCliente {
+                Id = Id,
+                ClienteId = ClienteId,
+                MotivoDesativacao = MotivoDesativacao,
+                Cliente = null,
+            };
+        }
+    }
+}
---
+++ BusinessModels/DesativacaoFuncionarioModel.cs
@@ -0,0 +1,25 @@
+﻿using BlurayDreamsAPI.Models;
+using Newtonsoft.Json;
+
+namespace BlurayDreamsAPI.BusinessModels
+{
+    public class DesativacaoFuncionarioModel
+    {
+        public int Id { get; set; }
+        public int FuncionarioId { get; set; }
+        public string MotivoDesativacao { get; set; }
+        [JsonIgnore]
+        public Funcionario? Funcionario { get; set; }
+
+        public DesativacaoFuncionario toEntity()
+        {
+            return new DesativacaoFuncionario
+            {
+                Id = Id,
+                FuncionarioId = FuncionarioId,
+                MotivoDesativacao = MotivoDesativacao,
+                Funcionario = null,
+            };
+        }
+    }
+}
