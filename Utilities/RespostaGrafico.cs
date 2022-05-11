f67c67e5091ee172846c40063e3d4e0cf4ffd4de
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-05-10 21:09:42 -0300

    query do googlecharts

---
+++ Utilities/RespostaGrafico.cs
@@ -0,0 +1,14 @@
+﻿namespace BlurayDreamsAPI.Utilities
+{
+    public class RespostaGrafico
+    {
+        public string Categoria { get; set; }
+        public List<Valor> Valores { get; set; }
+    }
+    public class Valor
+    {        
+        public DateTime Data { get; set; }
+        public int Quantidade { get; set; }
+    }
+
+}
